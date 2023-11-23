80ec6bfed47f82b55a9cb22f16acd51c10d8cc8b
jihad-hossain1 <jihadkhan934@gmail.com>
2023-11-23 13:30:05 +0600

    project query added with mongoose

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 6000;
+
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`server runnng on port ${port}`));
